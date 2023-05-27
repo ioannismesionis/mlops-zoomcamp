68b46bc409351c2fdb7de4337b355ad8878909c3
Atawua <atawua@Atawuas-MacBook-Pro.local>
2023-05-27 17:02:29 +0100

    assignment finished

--- cohorts/2023/02-experiment-tracking/homework/train.py
+++ cohorts/2023/02-experiment-tracking/homework/train.py
@@ -7,7 +7,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
 mlflow.set_tracking_uri("sqlite:///backend.db")  # I need to think if this is needed.
-mlflow.set_experiment("homework2-tracking")
+#mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("train-tracking")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -23,16 +24,19 @@ def load_pickle(filename: str):
 def run_train(data_path: str):
 
     # Add sklearn flavor autologging
-    mlflow.sklearn.autolog()
+    with mlflow.start_run():
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+        # Start autologging
+        mlflow.sklearn.autolog()
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
